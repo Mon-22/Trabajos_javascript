2c50b357406224cdb18154e5a63a9f6a4deee20f
Mon <chavezt.ms@gmail.com>
2025-05-29 21:19:40 -0700

    se abrieron los archivos de todos los ejercicios , estan pendientes de solicion

---
+++ ejercicio10.js
@@ -0,0 +1,9 @@
+/*
+
+    • Ejercicio 10 - Cálculo de Descuento: Escribe un programa que calcule el precio final de un producto después de aplicar un descuento. Pide al usuario que ingrese el precio original y el porcentaje de descuento, y muestra el precio final.
+Pista1: Investiga que es ParseFloat, ya que el ejercicio lo requiere
+Pista 2: La fórmula puede representarse asi:
+
+let montoDescuento = precioOriginal * (porcentajeDescuento / 100);
+
+*/
---
+++ ejercicio8.js
@@ -0,0 +1,13 @@
+/*
+    • Ejercicio 8 - Conversión de Temperaturas: Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit. La fórmula de conversión es: 
+
+f=Cx9/5+32
+
+Pide al usuario que ingrese la temperatura en Celsius y muestra el resultado en Fahrenheit.
+	Pista1: Investiga que es ParseFloat, ya que el ejercicio lo requiere
+Pista 2: La fórmula puede representarse asi: 
+let fahrenheit = celsius * 9 / 5 + 32; 
+
+
+*/
+
