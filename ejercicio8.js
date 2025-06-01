3869f384b9187ea3f3c555437edc61e4bb9f48ce
Mon <chavezt.ms@gmail.com>
2025-05-31 17:54:59 -0700

    ejercicio de conversion de grados

--- ejercicio8.js
+++ ejercicio8.js
@@ -1,5 +1,6 @@
 /*
-    • Ejercicio 8 - Conversión de Temperaturas: Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit. La fórmula de conversión es: 
+    • Ejercicio 8 - Conversión de Temperaturas: Escribe un programa que convierta una temperatura 
+    dada en grados Celsius a grados Fahrenheit. La fórmula de conversión es: 
 
 f=Cx9/5+32
 
@@ -8,6 +9,11 @@ Pide al usuario que ingrese la temperatura en Celsius y muestra el resultado en 
 Pista 2: La fórmula puede representarse asi: 
 let fahrenheit = celsius * 9 / 5 + 32; 
 
-
 */
+const prompt=require('prompt-sync')();
 
+let grados=parseFloat(prompt(' cuantos grados Celcius son los que deseas convertir:  ')); 
+
+let result= ((grados*9/5)+32); 
+
+console.log( grados, ' Celcius es igual a   ', + result, 'Fahrenheit ');
